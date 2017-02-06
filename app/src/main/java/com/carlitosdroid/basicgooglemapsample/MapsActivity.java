66deab0239460748bdaa2044788815bf3fd5c718
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2017-02-06 09:55:24 -0500

    wip(PermissionUtils): Adding permission feature

--- app/src/main/java/com/carlitosdroid/basicgooglemapsample/MapsActivity.java
+++ app/src/main/java/com/carlitosdroid/basicgooglemapsample/MapsActivity.java
@@ -1,9 +1,14 @@
 package com.carlitosdroid.basicgooglemapsample;
 
+import android.*;
+import android.content.pm.PackageManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.carlitosdroid.basicgooglemapsample.util.PermissionUtils;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,6 +18,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private GoogleMap mMap;
 
     @Override
@@ -38,10 +44,42 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        updateMyLocation();
+
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+    
+    private void updateMyLocation(){
+        if(!checkReady()){
+            return;
+        }
+
+        // Enable the location layer. Request the location permission if needed.
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // Uncheck the box until the layer has been enabled and request missing permission.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION, false);
+        }
+
+    }
+    
+    private boolean checkReady(){
+        if(mMap == null){
+            Toast.makeText(this, getString(R.string.map_not_ready), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+    
+
+
+
+
 }
--- app/src/main/java/com/carlitosdroid/basicgooglemapsample/util/PermissionUtils.java
+++ app/src/main/java/com/carlitosdroid/basicgooglemapsample/util/PermissionUtils.java
@@ -1,8 +1,35 @@
 package com.carlitosdroid.basicgooglemapsample.util;
 
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 /**
  * Created by carlos on 2/6/17.
+ *
  */
 
 public class PermissionUtils {
+
+    /**
+     * Requests the fine location permission. If a rationale with an additional explanation should
+     * be shown to the user, displays a dialog that triggers the request.
+     */
+    public static void requestPermission(AppCompatActivity activity, int requestId,
+                                         String permission, boolean finishActivity) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+            // Display a dialog with rationale.
+            Log.e("REQUESTP RATIONALE","RATIONALES");
+
+//            PermissionUtils.RationaleDialog.newInstance(requestId, finishActivity)
+//                    .show(activity.getSupportFragmentManager(), "dialog");
+        } else {
+            Log.e("NO RATIONALE","NO RATIONALE");
+
+            // Location permission has not been granted yet, request it.
+            ActivityCompat.requestPermissions(activity, new String[]{permission}, requestId);
+
+        }
+    }
+
 }
