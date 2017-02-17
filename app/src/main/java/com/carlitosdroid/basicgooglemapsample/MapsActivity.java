bbefa124d6ee1b7b44b501c9c5c14b4f07917804
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2017-02-17 01:01:27 -0500

    wip(MapsActivity): record audio button moved to center

--- app/src/main/java/com/carlitosdroid/basicgooglemapsample/MapsActivity.java
+++ app/src/main/java/com/carlitosdroid/basicgooglemapsample/MapsActivity.java
@@ -24,7 +24,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, OnClickLocationListener, View.OnClickListener {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, OnClickLocationListener,
+        View.OnClickListener {
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private static final int ALL_PERMISSION_REQUEST_CODE = 3;
@@ -64,6 +65,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
